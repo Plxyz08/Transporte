48488efaba05c1b9a88529344359acec7146cb44
sairarojas <136027108+sairarojas@users.noreply.github.com>
2023-10-17 17:58:42 -0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,13 @@
 import Main from "../components/Main.vue";
 import Buses from "../components/Buses.vue";
+import Login from "../components/Login.vue";
 
 import {createRouter, createWebHashHistory} from "vue-router"
 
 const routes=[
     {path:"/", component:Main},
-    {path:"/Buses", component:Buses}
+    {path:"/Buses", component:Buses},
+    {path:"/Login", component:Login},
 ]
 
 export const router = createRouter({
