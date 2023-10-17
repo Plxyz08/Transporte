631e8e0eedd1f86b2447bc2e67db3861aa0d7e40
sairarojas <136027108+sairarojas@users.noreply.github.com>
2023-10-17 17:59:18 -0500

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <button><router-link to="/Buses">Buses</router-link> </button>
+      <button><router-link to="/Login">Login</router-link> </button>
   </div>
 </template>
 <script setup></script>
