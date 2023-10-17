d957ee5f653488399770400038ba7b25cbf0d762
sairarojas <136027108+sairarojas@users.noreply.github.com>
2023-10-17 17:58:04 -0500

    Create login.vue

---
+++ src/components/login.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-parallax
+        src="https://cdn.quasar.dev/img/parallax1.jpg"
+        :height="150"
+      />
+
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
